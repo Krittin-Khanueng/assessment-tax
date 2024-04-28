d1d0da9d460643cfeae1ee5278de3894e9b85d5d
krittin\work <krittin@unixdev.co.th>
2024-04-28 20:03:10 +0700

    feat: add calculate wht

--- pkg/tax/service/taxServiceImpl.go
+++ pkg/tax/service/taxServiceImpl.go
@@ -1,7 +1,7 @@
 package service
 
 import (
-	"math"
+	"fmt"
 
 	_taxModel "github.com/Krittin-Khanueng/assessment-tax/pkg/tax/model"
 )
@@ -13,34 +13,65 @@ func NewTaxServiceImpl() TaxService {
 }
 
 func (s *TaxServiceImpl) CalculateTaxRefund(userInfo *_taxModel.UserInfo) (*_taxModel.Tax, error) {
-	personalDeduction := 60000.0
+	personalDeduction := s.convertBahtToSatang(60000.0)
 
 	var taxRefund float64
-	netAmount := userInfo.TotalIncome - personalDeduction
-	if netAmount <= 150000.0 {
-		taxRefund = 0.0
-		taxRefund = math.Round(taxRefund*100) / 100
+	netAmount := s.convertBahtToSatang(userInfo.TotalIncome) - personalDeduction
+
+	fmt.Printf("netAmount: %f\n", netAmount)
+	wat := s.convertBahtToSatang(userInfo.WHT)
+	if netAmount <= s.convertBahtToSatang(150000.0) {
+		taxRefund = s.calculateWht(netAmount, wat)
+		taxRefund = s.convertSatangToBaht(taxRefund)
 		return &_taxModel.Tax{Tax: taxRefund}, nil
 	}
 
-	if netAmount <= 500000.0 {
-		taxRefund = 0.10 * (netAmount - 150000.0)
-		taxRefund = math.Round(taxRefund*100) / 100
+	if netAmount <= s.convertBahtToSatang(500000.0) {
+		taxRefund = s.calculateWht(netAmount, wat)
+		taxRefund = s.convertSatangToBaht(taxRefund)
 		return &_taxModel.Tax{Tax: taxRefund}, nil
 	}
 
-	if netAmount <= 1000000.0 {
-		taxRefund = 35000.0 + (0.15 * (netAmount - 500000.0))
-		taxRefund = math.Round(taxRefund*100) / 100
+	if netAmount <= s.convertBahtToSatang(1000000.0) {
+		taxRefund = s.calculateWht(netAmount, wat)
+		taxRefund = s.convertSatangToBaht(taxRefund)
 		return &_taxModel.Tax{Tax: taxRefund}, nil
 	}
 
-	if netAmount <= 2000000.0 {
-		taxRefund = 35000.0 + 75000.0 + (0.20 * (netAmount - 1000000.0))
-		taxRefund = math.Round(taxRefund*100) / 100
+	if netAmount <= s.convertBahtToSatang(2000000.0) {
+		taxRefund = s.calculateWht(netAmount, wat)
+		taxRefund = s.convertSatangToBaht(taxRefund)
 		return &_taxModel.Tax{Tax: taxRefund}, nil
 	}
 	taxRefund = 35000.0 + 75000.0 + 200000.0 + (0.35 * (netAmount - 2000000.0))
-	taxRefund = math.Round(taxRefund*100) / 100
+	taxRefund = s.convertSatangToBaht(taxRefund)
 	return &_taxModel.Tax{Tax: taxRefund}, nil
+}
+
+func (s *TaxServiceImpl) calculateWht(netAmount, WHT float64) float64 {
+	fmt.Printf("netAmount: %f, WHT: %f\n", netAmount, WHT)
+	if netAmount <= s.convertBahtToSatang(150000.0) {
+		return WHT
+	}
+	if netAmount <= s.convertBahtToSatang(500000.0) {
+		return 0.10*(netAmount-s.convertBahtToSatang(150000.0)) - WHT
+	}
+	if netAmount <= 10000000.0 {
+		return 0.10*(5000000.0-1500000.0) + 0.15*(netAmount-5000000.0) - WHT
+	}
+
+	if netAmount <= 20000000.0 {
+		return 0.10*(5000000.0-1500000.0) + 0.15*(10000000.0-5000000.0) + 0.20*(netAmount-10000000.0) - WHT
+	}
+
+	return 350000.0 + 750000.0 + 2000000.0 + (0.35 * (netAmount - 20000000.0)) - WHT
+}
+
+func (s *TaxServiceImpl) convertSatangToBaht(satang float64) float64 {
+	return satang * 0.01
+
+}
+
+func (s *TaxServiceImpl) convertBahtToSatang(baht float64) float64 {
+	return baht * 100
 }
